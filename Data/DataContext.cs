54d2720a9a9ee303f34df2053c7becb6bee8dd78
Alexander Veselov <collabsis.g@gmail.com>
2023-04-23 20:14:12 +0400

    Добавлена возможность подключение к дефолтной базе данных

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,26 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Armstrong.Generator.Models;
 using Armstrong.Generator.Helpers;
-using Microsoft.Extensions.Configuration;
 
 namespace Armstrong.Generator.Data
 {
   public class DataContext : DbContext
   {
     public DbSet<Channel>? Channels { get; set; }
-    public DbSet<History>? Histories { get; set; }
+    // public DbSet<History>? Histories { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-      var config = SettingsHelper.GetConfiguration();
-      var sqlConfig = config.GetSection("SqlConnectionSettings")
-                            .Get<SqlConnectionSettings>();
+      // var _host = EnvironmentHelper.GetEnvirovmentVariable("DB_HOST");
+      // var _username = EnvironmentHelper.GetEnvirovmentVariable("DB_USER");
+      // var _pswd = EnvironmentHelper.GetEnvirovmentVariable("DB_PSWD");
+      // var _database = EnvironmentHelper.GetEnvirovmentVariable("DB_NAME");
+
+      var _host = "localhost";
+      var _username = "postgres";
+      var _pswd = "postgres";
+      var _database = "arms_webapp_development";
+
       var connectionStringBuilder = new Npgsql.NpgsqlConnectionStringBuilder
       {
-        Host = sqlConfig.Host,
-        Username = sqlConfig.Username,
-        Password = sqlConfig.Password,
-        Database = sqlConfig.Database
+        Host = _host,
+        Username = _username,
+        Password = _pswd,
+        Database = _database
       };
 
       options.UseNpgsql(connectionStringBuilder.ConnectionString);
--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
-﻿namespace Armstrong.Generator;
+﻿using Armstrong.Generator.Models;
+using Armstrong.Generator.Data;
+
+namespace Armstrong.Generator;
 class Program
 {
   static void Main(string[] args)
   {
+    Random random = new Random();
+
+    var context = new DataContext();
+    var channels = new List<Channel>();
+
+    channels = context.Channels.ToList<Channel>();
+
+    Console.WriteLine($"Found {channels.Count} channels");
+
+    while (true)
+    {
+      foreach (var channel in channels)
+      {
+        channel.EventSystemValue = random.NextDouble();
+        channel.EventDatetime = DateTime.UtcNow;
+        channel.UpdatedAt = DateTime.UtcNow;
+      }
+
+      context.SaveChanges();
+      Console.WriteLine($"Updated {channels.Count} channels");
+
+      Thread.Sleep(10000);
+    }
   }
 }
